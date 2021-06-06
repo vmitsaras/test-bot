c711be58b0b4af60a791dd647c6bd2a308b93a6a
Vasilis Mitsaras <me@vasileios-mitsaras.com>
2021-06-06 16:18:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_templates_and_slots
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
 customElements.define('element-details',
   class extends HTMLElement {
     constructor() {
