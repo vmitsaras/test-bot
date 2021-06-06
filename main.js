84a814e418a6f331b95629eb90b020106ca2bc3a
Vasilis Mitsaras <me@vasileios-mitsaras.com>
2021-06-06 19:23:22 +0200

    #5

--- main.js
+++ main.js
@@ -3,14 +3,14 @@
  * @todo Take over the world
  * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
  */
-customElements.define('element-details',
+customElements.define("element-details",
   class extends HTMLElement {
     constructor() {
       super();
       const template = document
-        .getElementById('element-details-template')
+        .getElementById("element-details-template")
         .content;
-      const shadowRoot = this.attachShadow({mode: 'open'})
+      const shadowRoot = this.attachShadow({mode: "open"})
         .appendChild(template.cloneNode(true));
     }
   }
